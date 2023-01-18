fc0325c36062a7b998971aca7db1ffec74b0c9ee
NatsuCamellia <natsucamellia@gmail.com>
2023-01-18 09:36:43 +0800

    Add Windows wallpaper painting

--- src/main/java/applemilktea3/init/ModPaintings.java
+++ src/main/java/applemilktea3/init/ModPaintings.java
@@ -11,6 +11,8 @@ public class ModPaintings {
     public static final DeferredRegister<PaintingVariant> PAINTING_VARIANTS =
             DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, AppleMilkTea3.MOD_ID);
 
+    public static final RegistryObject<PaintingVariant> WINDOWS =
+            PAINTING_VARIANTS.register("courbet", () -> new PaintingVariant(32, 16));
     public static final RegistryObject<PaintingVariant> PLANT =
             PAINTING_VARIANTS.register("plant", () -> new PaintingVariant(16, 16));
     public static final RegistryObject<PaintingVariant> WANDERER =
