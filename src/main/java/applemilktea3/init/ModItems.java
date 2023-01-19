2179329cc17d5623d1371e1d822589f009eb6c33
NatsuCamellia <natsucamellia@gmail.com>
2023-01-19 10:11:13 +0800

    Add 11 kinds of foods

--- src/main/java/applemilktea3/init/ModItems.java
+++ src/main/java/applemilktea3/init/ModItems.java
@@ -37,6 +37,18 @@ public class ModItems {
     public static final RegistryObject<Item> YUZU = ITEMS.register("yuzu",
             () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(4).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
 
+    public static final RegistryObject<Item> BAKED_APPLE = ITEMS.register("baked_apple", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(6).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+    public static final RegistryObject<Item> APPLE_TART = ITEMS.register("apple_tart", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(8).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+    public static final RegistryObject<Item> YUZU_MOUSSE = ITEMS.register("yuzu_mousse", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(8).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+    public static final RegistryObject<Item> SANDWICH_APPLE = ITEMS.register("sandwich_apple", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(6).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+    public static final RegistryObject<Item> SANDWICH_EGG = ITEMS.register("sandwich_egg", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(6).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+    public static final RegistryObject<Item> SANDWICH_YUZU = ITEMS.register("sandwich_yuzu", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(6).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+    public static final RegistryObject<Item> TOFFEE_APPLE = ITEMS.register("toffee_apple", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(5).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+    public static final RegistryObject<Item> TOFFEE_APPLE_ICY = ITEMS.register("toffee_apple_icy", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(6).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+    public static final RegistryObject<Item> TOFFEE_APPLE_WIND = ITEMS.register("toffee_apple_wind", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(6).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+    public static final RegistryObject<Item> TOFFEE_APPLE_GOLDEN = ITEMS.register("toffee_apple_golden", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(8).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+    public static final RegistryObject<Item> TOFFEE_APPLE_GREEN = ITEMS.register("toffee_apple_green", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(6).saturationMod(0.3f).build()).tab(ModCreativeModeTab.AMT3_TAB)));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
